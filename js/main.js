7f0da35f224e00ff885f70a5acc89bc8da973c2d
rafinurilaf <113412029+rafinurilaf@users.noreply.github.com>
2026-02-14 05:26:45 +0700

    new

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('MISEL IMUP').split('')
+    const titles = ('MISEL PRETTY').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
